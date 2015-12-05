7ea72488cc83c31b82c127c772f5d9cc9008f3c2
Kristof Houben <kristof@treeshadow.be>
2015-12-05 15:05:54 +0000

    Data model in a form

--- parse/cloud/app.js
+++ parse/cloud/app.js
@@ -10,21 +10,9 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+app.get('/comment', function(req, res) {
+  res.render('comment');
 });
-
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
 
 // Attach the Express app to Cloud Code.
 app.listen();
