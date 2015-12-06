679a08c05920c41e8f9f13106e748ebced3eb99f
Tom Claus <mail@tomclaus.be>
2015-12-06 01:29:11 +0000

    Lalalalalalalala

--- parse/cloud/app.js
+++ parse/cloud/app.js
@@ -91,69 +91,41 @@ app.get('/api/video-comments/:videoUrl?', function(req, res) {
 // Get ecommmerce for product
 // https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DsGbxmsDFVnE
 app.get('/api/ecommerce/:videoUrl?', function(req, res) {
-  
+
   // Parameters
   var videoUrl = req.param('videoUrl');
   videoUrl = decodeURIComponent(videoUrl);
+
+  console.log('OK');
+  console.log(videoUrl);
   
   // Get root comments
-  var rootCommentQuery = new Parse.Query('Comment');
-  rootCommentQuery.equalTo('videoUrl', videoUrl);
-  
+  var query = new Parse.Query('Product');
+  query.equalTo('productUrl', videoUrl);
+  /*
   // Get reply comments
-  var replyCommentQuery = new Parse.Query('Comment');
-  replyCommentQuery.matchesQuery('parentComment', rootCommentQuery);
+  var commentQuery = new Parse.Query('Comment');
+  commentQuery.matchesQuery('product', productQuery);
   
   // Create compound query
-  var query = Parse.Query.or(rootCommentQuery, replyCommentQuery);
+  var query = Parse.Query.or(commentQuery, commentQuery);*/
   query.include('user');
   query.include('product');
   query.ascending('createdAt');
   
   // Execute the query
   query.find().then(function(results) {
-    
-    var commentArray = [];
-    
-    for(var i=0; i<results.length; i++){
 
-      var isReply = false;
-      if(results[i].get('parentComment')){
-        isReply = true;
-      }
-      
-      var product = {};
-      if(results[i].get('product')){
-        product.url = results[i].get('product').get('productUrl');
-        product.title = results[i].get('product').get('title');
-        product.description = results[i].get('product').get('description');
-        product.image = results[i].get('product').get('imageUrl');
-      }
-      
-      // Create the comment
-      var commentObject = {
-        id: results[i].id,
-        timestamp: results[i].get('timestamp'),
-        description: results[i].get('description'),
-        isReply: isReply,
-        author: {
-          id: results[i].get('user').id,
-          username: results[i].get('user').get('username'),
-          picture: results[i].get('user').get('picture')
-        },
-        product: product
-      };
-      
-      // Push to the comment array
-      commentArray.push(commentObject);
-    }
-    
-    return commentArray;
+    console.log(results);
 
-  }).then(function(commentArray) {
+    var videoArray = [];
+
+    return videoArray;
+    
+  }).then(function(videoArray) {
     
     // Render template
-    res.render('video-comments', {comments: commentArray, videoUrl: videoUrl});
+    res.render('product-videos', {videos: videoArray, videoUrl: videoUrl});
     
   });
 
